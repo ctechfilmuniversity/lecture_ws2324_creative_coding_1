91e1d27971e42cf787c90d3dfd3c2dc5bd6eae52
Joel Schaefer <joel.schaefer@filmuniversitaet.de>
2023-11-07 16:06:25 +0100

    Update Full Homework Submission

--- docs/04_submissions/schaefer/02/main/sketch.js
+++ docs/04_submissions/schaefer/02/main/sketch.js
@@ -8,7 +8,7 @@ let Particles = []
 var img
 
   function preload() {
-  img = loadImage('assets/mycubes.png')
+    img = loadImage('assets/mycubes.png')
   }
 
   function setup() {
@@ -37,8 +37,11 @@ var img
     if (mouseIsPressed === true) {
       line(mouseX, mouseY, pmouseX, pmouseY)
       colorParticlesFromImage(img)
+      //save("staticPattern.png")
       for(let i in Particles){
         Particles[i].setPos(createVector(random(windowWidth),random(windowHeight)))
+        //Particles[i].drawLines(createVector(random(windowWidth),random(windowHeight)))
+
       }
     }
 
@@ -49,26 +52,17 @@ var img
   }
 
   function drawShapes() {
-    // first, we need to loop through all of the available poses
-    // that have been detected in the image
     for (var i = 0; i < poses.length; i++) {
       var currentPose = poses[i].pose
-      // we are now looping through the ARRAY of keypoints for
-      // this pose, and check each keypoint until we find the leftEye
       for (var j = 0; j < currentPose.keypoints.length; j++) {
         var keypoint = currentPose.keypoints[j]
-        // once we find the leftEye AND our confidence score is
-        // reasonably high, we're gonna draw a pink circle over it!
+
         if (keypoint.part === "nose" && keypoint.score > 0.2 ) {
-          //fill("magenta")
-          //noStroke()
           for(let i = 0; i< num;i++){     
             if(random(1)>0.8){
-            Particles[i].drawLines(createVector(keypoint.position.x,keypoint.position.y))
+              Particles[i].drawLines(createVector(keypoint.position.x,keypoint.position.y))
             }
           }
-
-          //ellipse(keypoint.position.x*2.5, keypoint.position.y*1.6, 20, 20)
         }
       }
     }
@@ -90,7 +84,6 @@ var img
   function updateParticles(){
     for(let i = 0; i< num;i++){
       Particles[i].update()
-      //Particles[i].drawLines(createVector(mouseX,mouseY))
     }
   }
 
