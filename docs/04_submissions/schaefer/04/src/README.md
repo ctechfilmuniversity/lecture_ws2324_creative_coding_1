2ffc9b7fe7f15fb73bbd9bf45ad43926a18189c6
Joel Schaefer <joel.schaefer@filmuniversitaet.de>
2023-11-22 17:34:56 +0100

    upload04

---
+++ docs/04_submissions/schaefer/04/src/README.md
@@ -0,0 +1,7 @@
+# Creative Coding I - Session 04, Task 02
+
+This is a very simple class project (*Creative Coding I*, MA Creative Technologies, Filmuniversität Babelsberg), utilizing the API for the pre-trained Hand model. It is doing a real-time hand pose detection in the browser with TensorFlow.js.
+
+We have added a randomly drawn lines between the tips of the fingers when two hands are detected.
+
+WIP
---
+++ docs/04_submissions/schaefer/04/src/helpers.js
@@ -0,0 +1,57 @@
+/**
+ * A simple function that returns the coordinates of the tip of a given finger
+ * for a given hand.
+ * 
+ * @param {Object} hand - Two hands of one person, or one hand per person if two.
+ * @param {*} index - Finger tip of index, middle ring finger or pinky.
+ * 
+ * @returns {Object} - Coordinates (x, y) of the finger tip.
+ */
+export function getFingerTipCoordinates(hand, index, canvasElement) {
+    if (hand) {
+        const handFingerTipX = hand[index].x * canvasElement.width;
+        const handFingerTipY = hand[index].y * canvasElement.height;
+
+        return {
+            x: handFingerTipX,
+            y: handFingerTipY
+        };
+    }
+}
+/**
+ * A simple function that draws a line between two finger tip coordinates.
+ * 
+ * @param {Object} coordsFingerHandOne - Coordinates (x, y) of the finger tip of first hand.
+ * @param {*} coordsFingerHandTwo - Coordinates (x, y) of the finger tip of second hand.
+ */
+export function drawLine(coordsFingerHandOne, coordsFingerHandTwo, canvasCtx) {
+    const controlPoint1 = getControlPoint(coordsFingerHandOne);
+    const controlPoint2 = getControlPoint(coordsFingerHandTwo);
+
+
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(coordsFingerHandOne.x, coordsFingerHandOne.y);
+    // Add two random control points -- bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+    canvasCtx.bezierCurveTo(controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, coordsFingerHandTwo.x, coordsFingerHandTwo.y);
+    canvasCtx.stroke();
+}
+
+/**
+ * A function that returns a random control point, near the given main point.
+ * 
+ * @param {Object} coordsFinger - Coordinates (x, y) of the main point.
+ * @returns Coordinates (x, y) of the control point.
+ */
+export function getControlPoint(coordsFinger) {
+    const coordsFingerX = coordsFinger.x - getRandomArbitrary(-50, 50);
+    const coordsFingerY = coordsFinger.y - getRandomArbitrary(-50, 200);
+
+    return {
+        x: coordsFingerX,
+        y: coordsFingerY,
+    };
+}
+
+export function getRandomArbitrary(min, max) {
+    return Math.random() * ((max - min) + min);
+}
